dc6794b064d2d1dc643d383e9c654c19eba2e98d
billiejoenahema <billiejoenahema@gmail.com>
2021-09-19 17:54:39 +0900

    make: css style

---
+++ src/components/AddNewModal.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="add-new-area">
+    <div class="input-area"></div>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="header">
+    <div class="app-name-wrapper">
+      <span>Todo App</span>
+    </div>
+    <div class="add-new">新規作成</div>
+    <AddNewModal v-if="isShowModal" />
+  </div>
+</template>
+
+<script>
+import AddNewModal from './AddNewModal.vue'
+export default {
+  components: {
+    AddNewModal
+  },
+  data() {
+    return {
+      isShowModal: false
+    }
+  }
+}
+</script>

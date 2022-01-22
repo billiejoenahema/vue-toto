edead2e77f193f570f789e85cfc7656ef529e257
billiejoenahema <billiejoenahema@gmail.com>
2022-01-22 10:54:55 +0900

    refactoring: apply <script setup>

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,33 +1,22 @@
+<script setup>
+import TodoFormModal from './TodoFormModal.vue';
+import { ref } from 'vue';
+
+const isShowModal = ref(false);
+const showTodoFormModal = () => {
+  isShowModal.value = true;
+};
+const hideTodoFormModal = () => {
+  isShowModal.value = false;
+};
+</script>
+
 <template>
   <div id="header">
     <div class="app-name-wrapper">
       <span>Todo App</span>
     </div>
-    <div class="add-new" @click="showAddNewModal" >新規作成</div>
-    <AddNewModal @closeModal="hideAddNewModal" v-if="shouldShowModal" />
+    <div class="add-new" @click="showTodoFormModal">新規作成</div>
+    <TodoFormModal @hideTodoFormModal="hideTodoFormModal" v-if="isShowModal" />
   </div>
 </template>
-
-<script>
-import AddNewModal from './AddNewModal.vue'
-import {mapGetters} from 'vuex'
-
-export default {
-  components: {
-    AddNewModal,
-  },
-  computed: {
-    ...mapGetters({
-      shouldShowModal: 'Todo/showAddNewModal',
-    }),
-  },
-  methods: {
-    showAddNewModal() {
-      this.$store.commit('Todo/toggleShowModal', true);
-    },
-    hideAddNewModal() {
-      this.$store.commit('Todo/toggleShowModal', false);
-    },
-  },
-}
-</script>

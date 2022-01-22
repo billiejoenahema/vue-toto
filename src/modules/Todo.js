9991c06d3b3b157777561e430d2e90ed1d2f6efb
billiejoenahema <billiejoenahema@gmail.com>
2022-01-22 10:55:22 +0900

    add: vuex action

--- src/modules/Todo.js
+++ src/modules/Todo.js
@@ -4,7 +4,7 @@ const initialNewTodoState = {
     processType: 1,
     priority: 1,
   },
-}
+};
 
 const state = {
   newTodo: {
@@ -15,57 +15,64 @@ const state = {
   todoList: [],
   showAddNewModal: false,
   editTargetIndex: 0,
-}
+};
 
 const getters = {
   newTodo: (state) => state.newTodo,
   todoList: (state) => state.todoList,
   showAddNewModal: (state) => state.showAddNewModal,
-}
+};
+
+const actions = {
+  addNewTodo({ commit }, data) {
+    commit('addNewTodo', data);
+  },
+};
 
 const mutations = {
   initNewTodo(state) {
-    state.newTodo = JSON.parse(JSON.stringify(initialNewTodoState.newTodo))
+    state.newTodo = JSON.parse(JSON.stringify(initialNewTodoState.newTodo));
   },
   toggleShowModal(state, isShow) {
-    state.showAddNewModal = isShow
+    state.showAddNewModal = isShow;
   },
   setNewTodoTitle(state, title) {
-    state.newTodo.title = title
+    state.newTodo.title = title;
   },
   setNewTodoPriority(state, priority) {
-    state.newTodo.priority = priority
+    state.newTodo.priority = priority;
   },
-  addNewTodo(state) {
-    state.todoList.push(state.newTodo)
+  addNewTodo(state, data) {
+    state.todoList.push(data);
   },
   updateStatus(state, { status, idx }) {
-    const target = state.todoList[idx]
-    target.processType = status
+    const target = state.todoList[idx];
+    target.processType = status;
   },
   todoSort(state, sortKey) {
     state.todoList.sort((a, b) => {
-      if (a[sortKey] < b[sortKey]) return -1
-      if (a[sortKey] > b[sortKey]) return 1
-    })
+      if (a[sortKey] < b[sortKey]) return -1;
+      if (a[sortKey] > b[sortKey]) return 1;
+    });
   },
   editTodo(state, index) {
-    const editTarget = state.todoList[index]
-    state.newTodo = { ...editTarget }
-    state.showAddNewModal = true
-    state.editTargetIndex = index
+    const editTarget = state.todoList[index];
+    state.newTodo = { ...editTarget };
+    state.showAddNewModal = true;
+    state.editTargetIndex = index;
   },
   updateTodo(state) {
-    state.todoList.splice(state.editTargetIndex, 1, state.newTodo)
+    state.todoList.splice(state.editTargetIndex, 1, state.newTodo);
   },
   deleteTodo(state, index) {
-    state.todoList = state.todoList.filter((todo, idx) => index !== idx)
+    state.todoList = state.todoList.filter((todo, idx) => index !== idx);
   },
-}
+};
 
 export default {
   namespaced: true,
   state,
   getters,
+  actions,
   mutations,
-}
+};

70b92c8e9cdf5c204c558c21ea74eabd90ea82da
billiejoenahema <billiejoenahema@gmail.com>
2022-01-30 20:40:41 +0900

    create: todo title tooltip

---
+++ src/components/TodoTitleTooltip.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  hoverTooltip: {
+    type: Function,
+    required: true,
+  },
+  leaveTooltip: {
+    type: Function,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="modal-todo-title-all"
+    @mouseover="hoverTooltip"
+    @mouseleave="leaveTooltip"
+  >
+    <div>{{ title }}</div>
+  </div>
+</template>

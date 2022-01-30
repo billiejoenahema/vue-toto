f9bb18a6022660e5cc08515ba46151e6ecd28833
billiejoenahema <billiejoenahema@gmail.com>
2022-01-30 21:21:01 +0900

    fix: styles of todo form

--- src/components/TodoFormModalEdit.vue
+++ src/components/TodoFormModalEdit.vue
@@ -34,7 +34,12 @@ const submitTodo = () => {
       <ion-icon class="close" @click="closeModal" name="close"></ion-icon>
       <div class="input-row">
         <label for="titleInput">タイトル</label>
-        <input v-model="todo.title" type="text" id="titleInput" />
+        <textarea
+          v-model="todo.title"
+          type="text"
+          rows="6"
+          id="titleInput"
+        ></textarea>
       </div>
       <div class="input-row">
         <label for="priorityType">優先度</label>
--- src/components/TodoFormModalNew.vue
+++ src/components/TodoFormModalNew.vue
@@ -27,7 +27,12 @@ const submitTodo = () => {
       <ion-icon class="close" @click="closeModal" name="close"></ion-icon>
       <div class="input-row">
         <label for="titleInput">タイトル</label>
-        <input v-model="newTodo.title" type="text" id="titleInput" />
+        <textarea
+          v-model="newTodo.title"
+          type="text"
+          rows="6"
+          id="titleInput"
+        ></textarea>
       </div>
       <div class="input-row">
         <label for="priorityType">優先度</label>

f9fb1fd9b02e0f1172dd4518a11f09b224bb59d5
billiejoenahema <billiejoenahema@gmail.com>
2022-02-05 21:21:57 +0900

    refactoring: cut out common parts as component wrappers

--- src/components/TodoFormModalNew.vue
+++ src/components/TodoFormModalNew.vue
@@ -2,6 +2,7 @@
 import { defineProps, reactive } from 'vue';
 import store from '../store/index.js';
 import { priorityTypes } from '../utilities.js';
+import TodoFormModalWrapper from './TodoFormModalWrapper';
 
 const props = defineProps({
   closeModal: {
@@ -22,35 +23,32 @@ const submitTodo = () => {
 </script>
 
 <template>
-  <div class="modal-wrapper" @click.self="closeModal">
-    <div class="modal">
-      <ion-icon class="close" @click="closeModal" name="close"></ion-icon>
-      <div class="input-row">
-        <label for="titleInput">タイトル</label>
-        <textarea
-          v-model="newTodo.title"
-          type="text"
-          rows="6"
-          id="titleInput"
-        ></textarea>
-      </div>
-      <div class="input-row">
-        <label for="priorityType">優先度</label>
-        <select v-model="newTodo.priority" id="priorityType">
-          <option
-            v-for="(type, idx) in priorityTypes"
-            :key="idx"
-            :value="type.priorityType"
-          >
-            {{ type.value }}
-          </option>
-        </select>
-      </div>
-      <div class="btn-wrapper">
-        <button class="submit" @click="submitTodo" :disabled="!newTodo.title">
-          確定
-        </button>
-      </div>
+  <TodoFormModalWrapper :closeModal="closeModal">
+    <div class="input-row">
+      <label for="titleInput">タイトル</label>
+      <textarea
+        v-model="newTodo.title"
+        type="text"
+        rows="6"
+        id="titleInput"
+      ></textarea>
     </div>
-  </div>
+    <div class="input-row">
+      <label for="priorityType">優先度</label>
+      <select v-model="newTodo.priority" id="priorityType">
+        <option
+          v-for="(type, idx) in priorityTypes"
+          :key="idx"
+          :value="type.priorityType"
+        >
+          {{ type.value }}
+        </option>
+      </select>
+    </div>
+    <div class="btn-wrapper">
+      <button class="submit" @click="submitTodo" :disabled="!newTodo.title">
+        確定
+      </button>
+    </div>
+  </TodoFormModalWrapper>
 </template>
---
+++ src/components/TodoFormModalWrapper.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  closeModal: {
+    type: Function,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="modal-wrapper" @click.self="closeModal">
+    <div class="modal">
+      <ion-icon class="close" @click="closeModal" name="close"></ion-icon>
+      <slot></slot>
+    </div>
+  </div>
+</template>

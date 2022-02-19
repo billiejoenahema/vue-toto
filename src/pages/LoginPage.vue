5956c92b55cbf054325559cd83fca52128e87fb7
billiejoenahema <billiejoenahema@gmail.com>
2022-02-19 20:24:03 +0900

    feat: input text length validation in login form

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { computed, reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import { useStore } from 'vuex';
+import { maxLength } from '../utilities';
 
 const store = useStore();
 const user = reactive({
@@ -18,15 +19,37 @@ const register = async () => {
     store.dispatch('User/register', user);
   }
 };
+const isDisabled = ref(true);
+const isOver = reactive({
+  userName: false,
+  password: false,
+});
+const checkLength = (prop) => {
+  maxLength[prop] < user[prop].length
+    ? (isOver[prop] = true)
+    : (isOver[prop] = false);
+};
 </script>
 
 <template>
   <form>
     <div class="column">
       <label for="userName">User Name</label>
-      <input id="userName" type="text" v-model="user.name" />
+      <input
+        id="userName"
+        type="text"
+        v-model="user.name"
+        :class="{ 'red-border': isOver.name }"
+        @keydown="checkLength('name')"
+      />
       <label for="password">Password</label>
-      <input id="password" type="password" v-model="user.password" />
+      <input
+        id="password"
+        type="password"
+        v-model="user.password"
+        :class="{ 'red-border': isOver.password }"
+        @keydown="checkLength('password')"
+      />
       <div
         v-show="errors"
         v-for="(error, index) in errors"
@@ -35,8 +58,12 @@ const register = async () => {
       >
         {{ error }}
       </div>
-      <div class="button login" @click="login">Login</div>
-      <div class="button register" @click="register">Register</div>
+      <div class="button login" @click="login" :disabled="isDisabled">
+        Login
+      </div>
+      <div class="button register" @click="register" :disabled="isDisabled">
+        Register
+      </div>
     </div>
   </form>
 </template>

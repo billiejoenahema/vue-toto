1cce7900333d2cd83a160b0baf4978403290326a
billiejoenahema <billiejoenahema@gmail.com>
2022-02-21 21:50:25 +0900

    test: progress bar sample

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,23 @@
+<ProgressBar :currentLength="current" :maxLength="max" />
+
+<script setup>
+import { defineProps, ref, watchEffect } from 'vue';
+const props = defineProps({
+  currentLength: {
+    type: Number,
+    required: true,
+  },
+  maxLength: {
+    type: Number,
+    required: true,
+  },
+});
+const progress = ref(0);
+watchEffect(() => {
+  progress.value = Math.round((props.currentLength / props.maxLength) * 100);
+});
+</script>
+
+<template>
+  <div :style="'width: ' + progress + '%'">{{ progress }} %</div>
+</template>
--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -1,15 +1,23 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
+import ProgressBar from '../components/ProgressBar';
 import TodoRow from '../components/TodoRow';
 
 const store = useStore();
 store.dispatch('Todo/get');
 const todoList = computed(() => store.getters['Todo/todoList']);
+const current = ref(0);
+const max = 100;
+const countUp = () => {
+  current.value += 1;
+};
 </script>
 
 <template>
   <div class="todo-row-wrapper">
+    <ProgressBar :currentLength="current" :maxLength="max" />
+    <button @click="countUp">+</button>
     <TodoRow
       v-for="(todo, idx) in todoList"
       :key="idx"

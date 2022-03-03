f6e643a7920a1a60288f93c3b6246bfe68c2d88d
billiejoenahema <billiejoenahema@gmail.com>
2022-03-03 20:07:12 +0900

    add: error messages constant

--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,7 @@
 export const priorityTypes = [
-  { priorityType: 3, value: '高' },
-  { priorityType: 2, value: '中' },
-  { priorityType: 1, value: '低' },
+  { type: 3, value: '高' },
+  { type: 2, value: '中' },
+  { type: 1, value: '低' },
 ];
 
 export const processTypes = [
@@ -10,8 +10,15 @@ export const processTypes = [
   { status: 3, value: '保留' },
 ];
 
-export const maxLength = {
+export const MAX_LENGTH = {
   title: 200,
   name: 50,
   password: 32,
+};
+
+export const ERROR_MESSAGE = {
+  title: '文字数オーバーです',
+  name: '名前は50文字以内で入力してください',
+  password: 'パスワードは32文字以内で入力してください',
+  login: 'ログインに失敗しました',
 };
